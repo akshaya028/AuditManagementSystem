7c3d6c53b0a50d5962d3572927f1f9e968c337ec
akshaya028 <56537937+akshaya028@users.noreply.github.com>
2021-10-18 10:36:49 +0530

    Add files via upload

---
+++ Audit Benchmark Service/Controllers/AuditBenchmarkController.cs
@@ -0,0 +1,34 @@
+﻿using Audit_Benchmark_Service.Models;
+using Audit_Benchmark_Service.Service_Layer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Audit_Benchmark_Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    //[Authorize]
+    [AllowAnonymous]
+    public class AuditBenchmarkController : ControllerBase
+    {
+        static readonly log4net.ILog _log4net = log4net.LogManager.GetLogger(typeof(AuditBenchmarkController));
+        // GET: api/<AuditBenchmarkController>
+        
+        [AllowAnonymous]
+        [HttpGet]
+        public List<AuditBenchmarkClass> Get()
+        {
+            _log4net.Info("In Audit benchmark");
+            BenchmarkService benchmarkService = new BenchmarkService();
+            _log4net.Info("List of benchmark is returned");
+            return benchmarkService.GetListOfBenchmarks();
+        }
+
+    }
+}
