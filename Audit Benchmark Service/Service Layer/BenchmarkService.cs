cf4404e3a60c4372c055e620fb28b7ec22fdf5cc
akshaya028 <56537937+akshaya028@users.noreply.github.com>
2021-10-18 10:38:24 +0530

    Add files via upload

---
+++ Audit Benchmark Service/Service Layer/BenchmarkService.cs
@@ -0,0 +1,22 @@
+﻿using Audit_Benchmark_Service.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Audit_Benchmark_Service.Service_Layer
+{
+    public class BenchmarkService
+    {
+        public virtual List<AuditBenchmarkClass> GetListOfBenchmarks()
+        {
+            List<AuditBenchmarkClass> AuditBenchmarkList = new List<AuditBenchmarkClass>()
+            {
+                new AuditBenchmarkClass(){ AuditType="Internal",BenchmarkNoAnswers=3 },
+                new AuditBenchmarkClass(){ AuditType="SOX",BenchmarkNoAnswers=1 },
+            };
+            return AuditBenchmarkList;
+        }
+
+    }
+}
